3c373920df973f189f3f5ea7bf667d0814c85d78
stephen <vincentstephen489@gmail.com>
2025-12-23 09:20:17 +0300

    create serializers which act as bridge between models and JSON.

---
+++ backend/bossyboss/accounts/serializers.py
@@ -0,0 +1,28 @@
+# Serializer is the bridge between complex data types such as querysets and Python data types that can be easily rendered into JSON, XML, or other content types.
+#Serializers are the bridge between models and JSON.
+# They define how model instances are converted to JSON and vice versa.
+
+from rest_framework import serializers
+from .models import User, CustomerProfile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'phone', 'full_name')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone', 'full_name', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class CustomerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerProfile
+        fields = '__all__'
