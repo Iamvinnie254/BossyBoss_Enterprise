67d73c907e2e7d2ebd7a57b7dce56823289d986e
stephen <vincentstephen489@gmail.com>
2025-12-24 15:44:50 +0300

    register and login endpoints  testing successfully

--- backend/bossyboss/accounts/views.py
+++ backend/bossyboss/accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "message": "User registered successfully",
+                    "email": user.email
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
